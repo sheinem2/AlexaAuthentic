486c581c299efc9a1d6f190f239fce8628dc4c4f
Heineman <sheinem2@LAMU02TC0FKGTFM.uhc.com>
2018-03-14 11:09:02 -0400

    First commit

---
+++ app/src/main/java/com/example/mchitrap/newauth/AlexaLogin.java
@@ -0,0 +1,13 @@
+package com.example.mchitrap.newauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlexaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alexa_login);
+    }
+}
